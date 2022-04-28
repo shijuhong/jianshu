a4a78b0318d3e8dc767dd32205101c45912620ca
shijuhong <940104683@qq.com>
2022-04-28 22:50:13 +0800

    忽略无法序列化报错的问题：。 报错内容：A non-serializable value was detected in the state

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./reducer";
 
-const store = configureStore({reducer});
+const store = configureStore({
+  reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
 
 export default store;
