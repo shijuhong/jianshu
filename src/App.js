c165ebfcfbb5fb6d630a72bbf46a44990a3bd19d
shijuhong <940104683@qq.com>
2022-04-30 20:27:51 +0800

    引入 react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import { Provider } from "react-redux";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import store from "./store";
 import Header from "./common/Header";
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
 
 function App() {
   return (
     <Provider store={store}>
       <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Home />}></Route>
+          <Route path="/detail" exact element={<Detail />}></Route>
+        </Routes>
+      </BrowserRouter>
     </Provider>
   );
 }
