39db5e0798340ace8121e5bce7a0466ee210389c
shijuhong <940104683@qq.com>
2022-05-01 11:05:19 +0800

    首页组件拆分

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,28 @@
 import React, { Component } from "react";
+import Topic from "./components/Topic";
+import List from "./components/List";
+import Recommend from "./components/Recommend";
+import Writer from "./components/Writer";
+import { HomeWrapper, HomeLeft, HomeRight } from "./style";
 
 export default class Home extends Component {
   render() {
-    return <div>Home</div>;
+    return (
+      <HomeWrapper>
+        <HomeLeft>
+          <img
+            className="banner-img"
+            src="https://upload.jianshu.io/admin_banners/web_images/5055/348f9e194f4062a17f587e2963b7feb0b0a5a982.png?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540"
+            alt=""
+          />
+          <Topic />
+          <List />
+        </HomeLeft>
+        <HomeRight>
+          <Recommend />
+          <Writer />
+        </HomeRight>
+      </HomeWrapper>
+    );
   }
 }
