ed65d4e81c0d45ac6c8fa8b0e10a5e399025007b
shijuhong <940104683@qq.com>
2022-05-01 16:03:33 +0800

    首页文章列表制作

--- src/pages/Home/components/List.js
+++ src/pages/Home/components/List.js
@@ -1,9 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from "react";
+import { connect } from "react-redux";
+import { ListItem, ListInfo } from "../style";
 
-export default class List extends Component {
+class List extends Component {
   render() {
+    const { articleList } = this.props;
     return (
-      <div>List</div>
-    )
+      <Fragment>
+        {articleList.map((item) => (
+          <ListItem key={item.get("id")}>
+            <img className="pic" src={item.get("imgUrl")} alt="" />
+            <ListInfo>
+              <h3 className="title">{item.get("title")}</h3>
+              <p className="desc">{item.get("desc")}</p>
+            </ListInfo>
+          </ListItem>
+        ))}
+      </Fragment>
+    );
   }
 }
+
+const mapState = (state) => ({
+  articleList: state.getIn(["home", "articleList"]),
+});
+
+export default connect(mapState)(List);
--- src/pages/Home/store/reducer.js
+++ src/pages/Home/store/reducer.js
@@ -16,6 +16,22 @@ const defaultState = fromJS({
         "https://upload.jianshu.io/publication/image/b3d2135b-3c0f-45c7-8428-6bc0d7539ca3?imageMogr2/auto-orient/strip|imageView2/2/w/125/format/webp",
     },
   ],
+  articleList: [
+    {
+      id: 1,
+      title: "读书让人安静",
+      desc: "今天阳光灿烂，蓝天高远。 拿出了前年参加这本书的译者王杨的新书发布会获得的这本书，再次打开来看。 这本书主要写了：当赖德军官看到那座庄园时，所有...",
+      imgUrl:
+        "https://upload-images.jianshu.io/upload_images/1699915-f4b8fe0aa734aa30.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240",
+    },
+    {
+      id: 2,
+      title: "读书让人安静",
+      desc: "今天阳光灿烂，蓝天高远。 拿出了前年参加这本书的译者王杨的新书发布会获得的这本书，再次打开来看。 这本书主要写了：当赖德军官看到那座庄园时，所有...",
+      imgUrl:
+        "https://upload-images.jianshu.io/upload_images/1699915-f4b8fe0aa734aa30.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240",
+    },
+  ],
 });
 
 const reducer = (state = defaultState, action) => {
