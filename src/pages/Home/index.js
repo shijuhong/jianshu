2c7782efef092a684a302309698d610189c51169
shijuhong <940104683@qq.com>
2022-05-01 21:07:56 +0800

    首页异步数据获取

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,8 +4,11 @@ import List from "./components/List";
 import Recommend from "./components/Recommend";
 import Writer from "./components/Writer";
 import { HomeWrapper, HomeLeft, HomeRight } from "./style";
+import axios from "axios";
+import { actionCreators } from "./store";
+import { connect } from "react-redux";
 
-export default class Home extends Component {
+class Home extends Component {
   render() {
     return (
       <HomeWrapper>
@@ -25,4 +28,26 @@ export default class Home extends Component {
       </HomeWrapper>
     );
   }
+  componentDidMount() {
+    const { changeHomeData } = this.props;
+    changeHomeData();
+  }
 }
+
+const mapDispatch = (dispatch) => ({
+  changeHomeData() {
+    axios.get("/api/home.json").then((res) => {
+      const result = res.data.data;
+      const action = {
+        type: "change_home_data",
+        topicList: result.topicList,
+        articleList: result.articleList,
+        recommendList: result.recommendList,
+        writerList: result.writerList,
+      };
+      dispatch(action);
+    });
+  },
+});
+
+export default connect(null, mapDispatch)(Home);
--- src/pages/Home/store/reducer.js
+++ src/pages/Home/store/reducer.js
@@ -2,70 +2,21 @@ import { constants } from ".";
 import { fromJS } from "immutable";
 
 const defaultState = fromJS({
-  topicList: [
-    {
-      id: 1,
-      title: "社会热点",
-      imgUrl:
-        "https://upload.jianshu.io/publication/image/b3d2135b-3c0f-45c7-8428-6bc0d7539ca3?imageMogr2/auto-orient/strip|imageView2/2/w/125/format/webp",
-    },
-    {
-      id: 2,
-      title: "社会热点",
-      imgUrl:
-        "https://upload.jianshu.io/publication/image/b3d2135b-3c0f-45c7-8428-6bc0d7539ca3?imageMogr2/auto-orient/strip|imageView2/2/w/125/format/webp",
-    },
-  ],
-  articleList: [
-    {
-      id: 1,
-      title: "读书让人安静",
-      desc: "今天阳光灿烂，蓝天高远。 拿出了前年参加这本书的译者王杨的新书发布会获得的这本书，再次打开来看。 这本书主要写了：当赖德军官看到那座庄园时，所有...",
-      imgUrl:
-        "https://img1.baidu.com/it/u=3331778458,1704878545&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=500",
-    },
-    {
-      id: 2,
-      title: "读书让人安静",
-      desc: "今天阳光灿烂，蓝天高远。 拿出了前年参加这本书的译者王杨的新书发布会获得的这本书，再次打开来看。 这本书主要写了：当赖德军官看到那座庄园时，所有...",
-      imgUrl:
-        "https://img2.baidu.com/it/u=3299975374,897725621&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=500",
-    },
-  ],
-  recommendList: [
-    {
-      id: 1,
-      imgUrl:
-        "https://sjh-picture.oss-cn-beijing.aliyuncs.com/img/20220501174944.png",
-    },
-    {
-      id: 2,
-      imgUrl:
-        "https://sjh-picture.oss-cn-beijing.aliyuncs.com/img/20220501175130.png",
-    },
-  ],
-  writerList: [
-    {
-      id: 1,
-      imgUrl:
-        "https://upload.jianshu.io/users/upload_avatars/7133325/f4370cf6-cf4d-4839-9b54-87beaa767d48?imageMogr2/auto-orient/strip|imageView2/1/w/96/h/96/format/webp",
-      name: "Hobbit霍比特人",
-      wordCount: 463.3,
-      likeCount: 2.3,
-    },
-    {
-      id: 2,
-      imgUrl:
-        "https://upload.jianshu.io/users/upload_avatars/5303650/33f2887c-e6e2-43b3-bd2c-b8516e5ee646.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/96/h/96/format/webp",
-      name: "Rose的肉丝儿",
-      wordCount: 147.1,
-      likeCount: 13.2,
-    },
-  ],
+  topicList: [],
+  articleList: [],
+  recommendList: [],
+  writerList: [],
 });
 
 const reducer = (state = defaultState, action) => {
   switch (action.type) {
+    case "change_home_data":
+      return state.merge({
+        topicList: fromJS(action.topicList),
+        articleList: fromJS(action.articleList),
+        recommendList: fromJS(action.recommendList),
+        writerList: fromJS(action.writerList),
+      });
     default:
       return state;
   }
