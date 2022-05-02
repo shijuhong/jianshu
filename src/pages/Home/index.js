5565f7852311b6f22b4f53d77f07f9df7271e4b8
shijuhong <940104683@qq.com>
2022-05-02 12:18:58 +0800

    返回顶部功能实现

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,12 +3,17 @@ import Topic from "./components/Topic";
 import List from "./components/List";
 import Recommend from "./components/Recommend";
 import Writer from "./components/Writer";
-import { HomeWrapper, HomeLeft, HomeRight } from "./style";
+import { HomeWrapper, HomeLeft, HomeRight, BackTop } from "./style";
 import { actionCreators } from "./store";
 import { connect } from "react-redux";
 
 class Home extends Component {
+  handleScrollTop() {
+    window.scrollTo(0, 0);
+  }
+
   render() {
+    const { showScroll } = this.props;
     return (
       <HomeWrapper>
         <HomeLeft>
@@ -24,19 +29,45 @@ class Home extends Component {
           <Recommend />
           <Writer />
         </HomeRight>
+        {showScroll && (
+          <BackTop onClick={this.handleScrollTop}>回到顶部</BackTop>
+        )}
       </HomeWrapper>
     );
   }
   componentDidMount() {
     const { changeHomeData } = this.props;
     changeHomeData();
+    this.bindEvents();
+  }
+
+  componentWillUnmount() {
+    // 组件销毁之前，事件要解绑
+    const { changeScrollTopShow } = this.props;
+    window.removeEventListener("scroll", changeScrollTopShow);
+  }
+
+  bindEvents() {
+    const { changeScrollTopShow } = this.props;
+    window.addEventListener("scroll", changeScrollTopShow);
   }
 }
+
+const mapState = (state) => ({
+  showScroll: state.getIn(["home", "showScroll"]),
+});
 
 const mapDispatch = (dispatch) => ({
   changeHomeData() {
     dispatch(actionCreators.getHomeInfo());
   },
+  changeScrollTopShow() {
+    if (document.documentElement.scrollTop > 400) {
+      dispatch(actionCreators.toggleTopShow(true));
+    } else {
+      dispatch(actionCreators.toggleTopShow(false));
+    }
+  },
 });
 
-export default connect(null, mapDispatch)(Home);
+export default connect(mapState, mapDispatch)(Home);
--- src/pages/Home/store/reducer.js
+++ src/pages/Home/store/reducer.js
@@ -7,22 +7,31 @@ const defaultState = fromJS({
   recommendList: [],
   writerList: [],
   articlePage: 1,
+  showScroll: false,
 });
+
+const changeHomeData = (state, action) =>
+  state.merge({
+    topicList: fromJS(action.topicList),
+    articleList: fromJS(action.articleList),
+    recommendList: fromJS(action.recommendList),
+    writerList: fromJS(action.writerList),
+  });
+
+const addArticleList = (state, action) =>
+  state.merge({
+    articleList: state.get("articleList").concat(action.list),
+    articlePage: action.nextPage,
+  });
 
 const reducer = (state = defaultState, action) => {
   switch (action.type) {
     case constants.CHANGE_HOME_DATA:
-      return state.merge({
-        topicList: fromJS(action.topicList),
-        articleList: fromJS(action.articleList),
-        recommendList: fromJS(action.recommendList),
-        writerList: fromJS(action.writerList),
-      });
+      return changeHomeData(state, action);
     case constants.ADD_ARTICLE_LIST:
-      return state.merge({
-        articleList: state.get("articleList").concat(action.list),
-        articlePage: action.nextPage,
-      });
+      return addArticleList(state, action);
+    case constants.TOGGLE_SCROLL_TOP:
+      return state.set("showScroll", action.show);
     default:
       return state;
   }
